b06828edba70a726a5f3a541ff13da0ff310edd2
SimoneSpirito <simospiri01@gmail.com>
2022-01-18 14:30:56 +0100

    Snack2

--- snack2/index.php
+++ snack2/index.php
@@ -1,5 +1,21 @@
 <?php 
 
-
+if(isset($_GET["name"])) {
+    if(isset($_GET["mail"])) {
+        if(isset($_GET["age"])) {
+            if(strlen($_GET["name"]) > 3 && (strpos($_GET["mail"], ".") != false) && (strpos($_GET["mail"], "@") != false) && is_numeric($_GET["age"])) {
+                echo "<h2>Accesso consentito</h2>";
+            } else {
+                echo "<h2>Accesso negato</h2>";
+            }
+        } else {
+            echo "<h2>Inserisci la tua età nella barra di ricerca</h2>";
+        }
+    } else {
+        echo "<h2>Inserisci la tua email nella barra di ricerca</h2>";
+    }
+} else {
+    echo "<h2>Inserisci il tuo nome nella barra di ricerca</h2>";
+}
 
 ?>
